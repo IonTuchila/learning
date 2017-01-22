ec043a37dce1f463f514ef04720a97f57398f926
Ion <Ion>
2017-01-22 23:42:39 +0200

    first commit

---
+++ app/src/main/java/com/example/e7450/bussinesapp/FullImageActivity.java
@@ -0,0 +1,8 @@
+package com.example.e7450.bussinesapp;
+
+/**
+ * Created by E7450 on 1/20/2017.
+ */
+
+public class FullImageActivity {
+}
---
+++ app/src/main/res/ImageAdapter.java
@@ -0,0 +1,6 @@
+/**
+ * Created by E7450 on 1/18/2017.
+ */
+
+public class ImageAdapter {
+}
